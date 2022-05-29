1c3636b343877278cafddb0a1b7aeef972e0776a
rodolphe37 <rodolphe.a@gmail.com>
2022-05-29 12:01:43 +0200

    added base app, created some folders & added all pictures

---
+++ src/assets/picturesExport.js
@@ -0,0 +1,17 @@
+import AlipineIcon from "./sportCars/Alpine-Logo.png";
+import BugattiIcon from "./sportCars/Bugatti-Logo-1024x768.png";
+import FerrariIcon from "./sportCars/Ferrari-Logo-1024x768.png";
+import LamborghiniIcon from "./sportCars/Lamborghini-Logo-1024x768.png";
+import GeometryIcon from "./electricCars/logo-Geometry-A.png";
+import KarmaIcon from "./electricCars/Karma-Logo.png";
+import TeslaIcon from "./electricCars/Tesla_Logo.png";
+
+export {
+  AlipineIcon,
+  BugattiIcon,
+  FerrariIcon,
+  LamborghiniIcon,
+  GeometryIcon,
+  KarmaIcon,
+  TeslaIcon,
+};
